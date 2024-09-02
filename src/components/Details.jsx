4fa33fa036eda97f663bbe210641be4062f7d683
sami855-ux <samitale56@outlook.com>
2024-09-02 12:49:53 +0500

    Static version of the Details Section

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types"
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa"
 
 function Details() {
   return (
-    <div className="w-screen h-screen pl-32 py-20">
+    <div className="w-screen h-screen pl-32 py-20 flex">
       <Deals />
+      <DealsImage />
     </div>
   )
 }
@@ -49,9 +51,74 @@ function DisplayTime({ time, status }) {
       <div className="w-[90%] h-16 flex  items-center justify-center shadow-md space-y-4">
         <span className="text-3xl">{time}</span>
       </div>
-      <span className="pt-2">{status}</span>
+      <span className="pt-2 font-semibold">{status}</span>
     </section>
   )
 }
 
+function DealsImage() {
+  return (
+    <div className="w-[900px] h-full  flex">
+      <Btns />
+      <ImageWrapper />
+    </div>
+  )
+}
+
+function Btns() {
+  return (
+    <section className="w-32 h-full  flex items-end justify-center py-2 space-x-2">
+      <span className="w-10 h-10 rounded-full bg-slate-100 shadow-md flex justify-center items-center cursor-pointer">
+        <FaAngleLeft size={25} />
+      </span>
+      <span className="w-10 h-10 rounded-full bg-slate-100 shadow-md flex justify-center items-center cursor-pointer">
+        <FaAngleRight size={25} />
+      </span>
+    </section>
+  )
+}
+
+function ImageWrapper() {
+  return (
+    <>
+      <div className="w-[350px] h-full relative ">
+        <img
+          src="public/img/shoe6.jfif"
+          alt="Clothe-One"
+          className="w-full h-full object-cover"
+        />
+        <section className="w-[220px] h-36 bg-slate-100 absolute bottom-5 left-5 rounded-md p-4">
+          <h2 className="text-lg pb-7 text-gray-800 font-thin">
+            01 ----- Spring Safe
+          </h2>
+          <span className="text-4xl ">30% OFF</span>
+        </section>
+      </div>
+
+      <div className="w-[380px] h-full flex flex-col  ml-5">
+        <div className="w-full h-[80%] bg-slate-100">
+          <img
+            src="public/img/cloth10.jfif"
+            alt=""
+            className="w-full h-full mix-blend-plus-darker"
+          />
+        </div>
+        <section className="w-full h-16 flex space-x-2 items-end">
+          <span className="w-6 h-6 rounded-full flex justify-center items-center border border-gray-900">
+            <button className="w-2 h-2 rounded-full bg-gray-900"></button>
+          </span>
+          <span className="w-6 h-6 rounded-full flex justify-center items-center  border-gray-900">
+            <button className="w-2 h-2 rounded-full bg-gray-900"></button>
+          </span>
+          <span className="w-6 h-6 rounded-full flex justify-center items-center  border-gray-900">
+            <button className="w-2 h-2 rounded-full bg-gray-900"></button>
+          </span>
+          <span className="w-6 h-6 rounded-full flex justify-center items-center  border-gray-900">
+            <button className="w-2 h-2 rounded-full bg-gray-900"></button>
+          </span>
+        </section>
+      </div>
+    </>
+  )
+}
 export default Details
