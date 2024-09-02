bbbe66c351c970491eb83a905ff5cc0627b880e3
sami855-ux <samitale56@outlook.com>
2024-09-02 12:58:16 +0500

    Image source as a file

---
+++ src/components/imgDate-1.js
@@ -0,0 +1,39 @@
+const images = [
+  {
+    id: "111",
+    imgPath: "public/img/shoe2.jfif",
+    alt: "Shoe-image One",
+  },
+  {
+    id: "222",
+    imgPath: "public/img/shoe3.jfif",
+    alt: "Shoe-image Two",
+  },
+  {
+    id: "333",
+    imgPath: "public/img/shoe4.jfif",
+    alt: "Shoe-image Three",
+  },
+  {
+    id: "444",
+    imgPath: "public/img/shoe5.jfif",
+    alt: "Shoe-image Four",
+  },
+  {
+    id: "555",
+    imgPath: "public/img/shoe6.jfif",
+    alt: "Shoe-image Five",
+  },
+  {
+    id: "666",
+    imgPath: "public/img/shoe8.jfif",
+    alt: "Shoe-image Six",
+  },
+  {
+    id: "777",
+    imgPath: "public/img/shoe9.jfif",
+    alt: "Shoe-image Seven",
+  },
+]
+
+export default images
