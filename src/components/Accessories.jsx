e996199fe0333e217e581aed625ddd704cd73450
sami855-ux <samitale56@outlook.com>
2024-09-03 10:22:53 +0500

    Accessories section is added

--- src/components/Accessories.jsx
+++ src/components/Accessories.jsx
@@ -1,5 +1,17 @@
 function Accessories() {
-  return <div className=""></div>
+  return (
+    <div
+      className="w-screen h-screen bg-slate-100 flex justify-center items-center flex-col py-20"
+      id="access-wrapper"
+    >
+      <h2 className="font-semibold text-6xl text-slate-100">
+        Spring/Summer Accessories
+      </h2>
+      <button className="py-2 mt-10 w-56 text-slate-100 bg-transparent border border-slate-100 rounded-3xl transition-all duration-200 ease-in-out hover:bg-slate-100 hover:text-gray-800">
+        Shop Now
+      </button>
+    </div>
+  )
 }
 
 export default Accessories
