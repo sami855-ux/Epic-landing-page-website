7674457342a27bd2dc37b706779d3a355f48d208
sami855-ux <samitale56@outlook.com>
2024-09-03 12:51:39 +0500

    Service section is added

---
+++ src/components/Services.jsx
@@ -0,0 +1,17 @@
+function Services() {
+  return (
+    <div
+      className="w-screen h-[80vh] bg-slate-100 flex justify-center items-center flex-col py-20"
+      id="Service-wrapper"
+    >
+      <h2 className="font-semibold text-6xl text-slate-100">
+        Louis Vuitton Services
+      </h2>
+      <button className="py-2 mt-10 w-56 text-slate-100 bg-transparent border border-slate-100 rounded-3xl transition-all duration-200 ease-in-out hover:bg-slate-100 hover:text-gray-800">
+        Explore More
+      </button>
+    </div>
+  )
+}
+
+export default Services
