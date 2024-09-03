d87f805c079200dec4fabc957b0b38c94959d4b4
sami855-ux <samitale56@outlook.com>
2024-09-03 10:00:35 +0500

    Date for second slider

---
+++ src/components/imgDate-2.js
@@ -0,0 +1,34 @@
+const ImageDateTwo = [
+  {
+    id: "111",
+    imgPath: "public/img/cloth7.jfif",
+    alt: "Shoe-image One",
+  },
+  {
+    id: "222",
+    imgPath: "public/img/cloth9.jfif",
+    alt: "Shoe-image Two",
+  },
+  {
+    id: "333",
+    imgPath: "public/img/cloth10.jfif",
+    alt: "Shoe-image Three",
+  },
+  {
+    id: "444",
+    imgPath: "public/img/cloth11.jfif",
+    alt: "Shoe-image Four",
+  },
+  {
+    id: "555",
+    imgPath: "public/img/cloth13.jfif",
+    alt: "Shoe-image Five",
+  },
+  {
+    id: "666",
+    imgPath: "public/img/cloth14.jfif",
+    alt: "Shoe-image Six",
+  },
+]
+
+export default ImageDateTwo
