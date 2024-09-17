32ffb85c46d97b896568f096fed76cfc88c8c2d1
sami855-ux <samitale56@outlook.com>
2024-09-17 21:10:28 +0500

    Footer section is added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+function Footer() {
+  return (
+    <div className="w-full h-[40vh] bg-gray-100 flex space-x-14 px-20 py-11">
+      <section className="w-[250px] h-full mr-6">
+        <h2 className="font-semibold text-4xl py-1 text-gray-700">
+          LOUIS VUITTON
+        </h2>
+        <p className="text-sm pt-1">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum soluta
+          incidunt cumque nemo omnis consequuntur nostrum dolore aspernatur.
+          Laudantium repellendus enim provident. <br /> <br /> Cupiditate culpa
+          saepe asperiores veniam alias.
+        </p>
+      </section>
+
+      <section className="h-full w-[150px]">
+        <h2 className="font-semibold text-lg py-3 uppercase mr-6 ">company</h2>
+        <p className="pb-2">About</p>
+        <p className="pb-2">Features</p>
+        <p className="pb-2">Weeks</p>
+        <p className="pb-2">Conatct</p>
+      </section>
+      <section className="h-full w-[150px]">
+        <h2 className="font-semibold text-lg py-3 uppercase mr-6 ">help</h2>
+        <p className="pb-2">Customer Support</p>
+        <p className="pb-2">Dealivery Details</p>
+        <p className="pb-2">Terms and conditions</p>
+        <p className="pb-2">Aggrements</p>
+      </section>
+      <section className="h-full w-[150px]">
+        <h2 className="font-semibold text-lg py-3 uppercase mr-6 ">FAQ</h2>
+        <p className="pb-2">Account</p>
+        <p className="pb-2">Manager Delivenes</p>
+        <p className="pb-2">Employees</p>
+        <p className="pb-2">Othres</p>
+      </section>
+      <section className="h-full w-[150px]">
+        <h2 className="font-semibold text-lg py-3 uppercase mr-6 ">
+          Resources
+        </h2>
+        <p className="pb-2">Free eBooks</p>
+        <p className="pb-2">Development Tutorial</p>
+        <p className="pb-2">How to - Blog</p>
+        <p className="pb-2">Website Development</p>
+      </section>
+    </div>
+  )
+}
+
+export default Footer
